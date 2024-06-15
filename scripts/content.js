0fb612902d849259ac3d462515eb1f105c6f39cd
rdata08 <rg673@cornell.edu>
2024-06-14 19:12:08 -0700

    Update permissions

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,12 @@
-const 
+const website = 'https://www.amazon.com';
+
+chrome.tabs.query({ url: website }, async (tabs) => {
+    if (tabs.length > 0) {
+        const tab = tabs[0];
+        await chrome.scripting.insertCSS({
+            target: { tabID: tab.id },
+            files: ['clean.css']
+        });
+    }
+})
+
