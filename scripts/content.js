e9784c666326156a30d7cace4ed3d2ca4e8c0d5c
rdata08 <rg673@cornell.edu>
2024-06-15 17:07:00 -0700

    Update html matches & content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,12 @@
 const searchBar = document.getElementById('nav-search-bar-form')
 
-if (searchBar) {
-    document.body.innerHTML = '';
-    const container = document.createElement('div');
-    container.style.display = 'flex';
-    container.style.justifyContent = 'center';
-    container.style.alignItems = 'center';
-    container.style.height = '100vh';
+document.body.innerHTML = '';
+const container = document.createElement('div');
+container.style.display = 'flex';
+container.style.justifyContent = 'center';
+container.style.alignItems = 'center';
+container.style.height = '100vh';
 
-    container.appendChild(searchBar);
-    document.body.appendChild(container);
-}
+container.appendChild(searchBar);
+document.body.appendChild(container);
+
