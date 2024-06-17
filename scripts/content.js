b0059597ac3d411226b859323b94e0da83f5161c
rdata08 <rg673@cornell.edu>
2024-06-17 13:57:57 -0700

    Updated Landing Page

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,32 @@
-const searchBar = document.getElementById('nav-search-bar-form')
+//Cadabra Logo
+const image = document.createElement('img');
+image.src = 'images/cadabra-logo.png'
 
+//Create new div for image
+const imgContainer = document.createElement('div');
+
+//Append img to imgContainer
+imgContainer.appendChild(image);
+
+//Search Bar
+const searchBar = document.getElementById('nav-search-bar-form');
+
+//Clears body content -------------------------------------------------------------------------
 document.body.innerHTML = '';
+
+//Create div to store search bar
 const container = document.createElement('div');
 container.style.display = 'flex';
 container.style.justifyContent = 'center';
 container.style.alignItems = 'center';
 container.style.height = '100vh';
+container.style.color = 'white';
 
+//Append searchBar to container
 container.appendChild(searchBar);
+
+//Append containers to document
+document.body.appendChild(imgContainer)
 document.body.appendChild(container);
 
 document.documentElement.style.display = 'block'; 
-
-
-
-
