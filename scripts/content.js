0090be13d01961f054e851eb016ae74c962b5769
rdata08 <rg673@cornell.edu>
2024-06-17 18:30:40 -0700

    Updating Landing Page

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,25 @@
-//Cadabra Logo
-const image = document.createElement('img');
-image.src = 'images/cadabra-logo.png'
-
-//Create new div for image
-const imgContainer = document.createElement('div');
-
-//Append img to imgContainer
-imgContainer.appendChild(image);
-
 //Search Bar
 const searchBar = document.getElementById('nav-search-bar-form');
 
 //Clears body content -------------------------------------------------------------------------
 document.body.innerHTML = '';
+
+// var imageURL = chrome.runtime.getURL("images/cadabra-logo.png")
+
+//Cadabra Logo
+const image = document.createElement('img');
+image.src = imageURL;
+image.alt = 'Cadabra Logo';
+
+//Create new div for image
+const imgContainer = document.createElement('div');
+imgContainer.style.display = 'flex';
+imgContainer.style.justifyContent = 'center';
+imgContainer.style.alignItems = 'center';
+imgContainer.style.height = '1000';
+
+//Append img to imgContainer
+imgContainer.appendChild(image);
 
 //Create div to store search bar
 const container = document.createElement('div');
@@ -26,7 +33,7 @@ container.style.color = 'white';
 container.appendChild(searchBar);
 
 //Append containers to document
-document.body.appendChild(imgContainer)
+document.body.appendChild(imgContainer);
 document.body.appendChild(container);
 
 document.documentElement.style.display = 'block'; 
