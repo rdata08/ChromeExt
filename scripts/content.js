1bad1eb5e3561ecb9e31be4805e51a7ea521ef1b
rdata08 <rg673@cornell.edu>
2024-06-20 13:23:59 -0700

    Update code clarity

--- scripts/content.js
+++ scripts/content.js
@@ -15,31 +15,30 @@ image.width = 800;
 image.height = 800;
 
 //Create new div for image
-const imgContainer = document.createElement('div');
-imgContainer.style.display = 'flex';
-imgContainer.style.flexDirection = 'column';
-imgContainer.style.justifyContent = 'center';
-imgContainer.style.alignItems = 'center';
-imgContainer.appendChild(image);  //Append img to imgContainer
+const imgDiv = document.createElement('div');
+imgDiv.style.display = 'flex';
+imgDiv.style.flexDirection = 'column';
+imgDiv.style.justifyContent = 'center';
+imgDiv.style.alignItems = 'center';
+imgDiv.appendChild(image);  //Append img to imgContainer
 
 //Create div to store search bar
-const container = document.createElement('div');
-container.style.display = 'flex';
-container.style.flexDirection = 'column';
-container.style.color = 'white';
-container.appendChild(searchBar);  //Append searchBar to container
+const searchBarDiv = document.createElement('div');
+searchBarDiv.style.display = 'flex';
+searchBarDiv.style.flexDirection = 'column';
+searchBarDiv.style.color = 'white';
+searchBarDiv.appendChild(searchBar);  //Append searchBar to container
 
 //Body div
 const body = document.createElement('div');
 body.style.display = 'flex';
 body.style.flexDirection = 'column';
 body.style.justifyContent = 'center';
-body.style.alignItems = 'center';
 body.style.height = '100vh';
 
 //Body div nests both divs inside
-body.appendChild(imgContainer);
-body.appendChild(container);
+body.appendChild(imgDiv);
+body.appendChild(searchBarDiv);
 
 //Append body div to document
 document.body.appendChild(body);
--- scripts/sponsored.js
+++ scripts/sponsored.js
@@ -1,3 +1,5 @@
+// Given a selector, and ad type, removes ads from search pages.
+// Logs number of ads removed and ad type in console.
 function removeAd (selector, adType) {
     const removedElements = document.querySelectorAll(selector);
     
@@ -15,6 +17,6 @@ removeAd('div[class*="s-flex-geom', 'video');  //video ads
 removeAd('div[class*="copilot-secure-display"]', 'behavioral')  //behavioral ads
 
 // Keep next page bar
-const nextPageBar = document.getElementById('a-section a-spacing-none s-result-item s-flex-full-width s-widget s-widget-spacing-large');
+// const nextPageBar = document.getElementById('a-section a-spacing-none s-result-item s-flex-full-width s-widget s-widget-spacing-large');
 
-document.
+// document.
