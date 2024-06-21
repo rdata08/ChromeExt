8345967921840bdd1a399a3cd363a6342900b7d5
rdata08 <rg673@cornell.edu>
2024-06-21 14:57:38 -0700

    Updating Landing Page - search bar

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -1,5 +1,13 @@
+//Finds Delivery Button
+const deliverToButton = document.getElementById('nav-global-location-slot');
+deliverToButton.style.cssText = '';
+
 //Search Bar
-const searchBar = document.getElementById('nav-belt');
+const searchBar = document.getElementById('nav-search-bar-form');
+
+//Language Option 
+
+const languageButton = document.getElementById('icp-nav-flyout');
 
 //Clears body content -------------------------------------------------------------------------
 document.body.innerHTML = '';
@@ -22,13 +30,6 @@ imgDiv.style.justifyContent = 'center';
 imgDiv.style.alignItems = 'center';
 imgDiv.appendChild(image);  //Append img to imgContainer
 
-//Create div to store search bar
-const searchBarDiv = document.createElement('div');
-searchBarDiv.style.display = 'flex';
-searchBarDiv.style.flexDirection = 'column';
-searchBarDiv.style.color = 'white';
-searchBarDiv.appendChild(searchBar);  //Append searchBar to container
-
 //Body div
 const body = document.createElement('div');
 body.style.display = 'flex';
@@ -36,9 +37,46 @@ body.style.flexDirection = 'column';
 body.style.justifyContent = 'center';
 body.style.height = '100vh';
 
+//New search bar
+const searchBarDisplay = document.createElement('div');
+
+const deliverTo = document.createElement('div');
+const search = document.createElement('div');
+const language = document.createElement('div');
+const orders = document.createElement('div');
+const cart = document.createElement('div');
+
+//new class for searchBarDisplay
+searchBarDisplay.classList.add('searchBarDisplay');
+searchBarDisplay.style.display = 'flex';
+searchBarDisplay.style.flexDirection = 'row';
+searchBarDisplay.style.justifyContent = 'flex-start';
+searchBarDisplay.style.alignItems = 'center';
+searchBarDisplay.style.alignSelf = 'center';
+
+//Deliver Button
+deliverTo.classList.add('deliverTo');
+deliverTo.appendChild(deliverToButton);
+
+//Search Bar Form
+search.classList.add('searchBar');
+search.appendChild(searchBar);
+
+language.classList.add('language')
+language.appendChild(languageButton);
+
+searchBarDisplay.appendChild(deliverTo);
+searchBarDisplay.appendChild(search);
+searchBarDisplay.appendChild(language);
+searchBarDisplay.appendChild(orders);
+searchBarDisplay.appendChild(cart);
+
+
 //Body div nests both divs inside
 body.appendChild(imgDiv);
-body.appendChild(searchBarDiv);
+body.appendChild(searchBarDisplay);
+// body.appendChild(deliverToButton);
+// body.appendChild(searchBarDiv);
 
 //Append body div to document
 document.body.appendChild(body);
