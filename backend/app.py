300020976383eddab05f62d20ad0123784a5c814
rdata08 <rg673@cornell.edu>
2024-06-25 20:40:37 -0700

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+# Gets total spent in the last 30 days
+@app.route('/price', methods=['GET'])
+def fetch_total():
+    url = ''
