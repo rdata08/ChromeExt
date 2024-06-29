26f68cba6a577100e1223de6bd6fc2f9a46abe79
rdata08 <rg673@cornell.edu>
2024-06-29 15:15:57 -0700

    Playing around backend functionalities

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,25 @@
 from flask import Flask, jsonify
 import requests
+from pprint import pprint
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-# Gets total spent in the last 30 days
-@app.route('/price', methods=['GET'])
-def fetch_total():
-    url = ''
+@app.route('/')
+def scrapeOne():
+    url = 'https://www.amazon.com/Bose-QuietComfort-45-Bluetooth-Canceling-Headphones/dp/B098FKXT8L'
+    # no headers at the moment
     
+    session = requests.Session()
+    
+    r = session.get(url)
+    if r.status_code == 200:
+        return jsonify(status = r.status_code, content = r.text)
+    elif r.status_code == 503:
+        print("503: Status Code")
+    else:
+        print(f"Unexpected: {r.status_code} Status Code")
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
