c2531352b5a789e2c557ba902a9b5876c2b358e5
rdata08 <rg673@cornell.edu>
2024-07-08 19:07:08 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,19 +7,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def scrapeOne():
-    url = 'https://www.amazon.com/Bose-QuietComfort-45-Bluetooth-Canceling-Headphones/dp/B098FKXT8L'
+    url = 'https://www.amazon.com/gp/css/order-history'
     # no headers at the moment
     
     session = requests.Session()
     
     r = session.get(url)
+
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    title_element = soup.select_one("#product-summary")
+    if title_element:
+        title = title_element.text.strip()
+    else: 
+        None
+
+    payload =  {
+        'title': title
+    }
+
     if r.status_code == 200:
-        return jsonify(status = r.status_code, content = r.text)
+        return soup
     elif r.status_code == 503:
         print("503: Status Code")
     else:
         print(f"Unexpected: {r.status_code} Status Code")
-    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
